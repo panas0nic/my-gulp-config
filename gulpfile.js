68c9b2f34618113597351b51f108ec8fe415069c
Andrey Panasenko <honeywithandrew@gmail.com>
2018-11-20 14:32:54 +0300

    Added gulp-notify and gulp-plumber
    Обработка ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
 
 /* Задачи для Gulp */
 gulp.task('server', function() {
@@ -17,6 +19,14 @@ gulp.task('server', function() {
 
 gulp.task('less', function() {
     return gulp.src('./src/less/main.less')
+    	.pipe(plumber({
+    		errorHandler: notify.onError(function(err){
+    			return {
+    				title: 'Styles',
+    				message: err.message
+    			}
+    		})
+    	}))
     	.pipe(less())
     	.pipe(gulp.dest('./src/css'))
     	.pipe(browserSync.stream());
