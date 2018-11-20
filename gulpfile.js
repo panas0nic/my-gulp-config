7fa95f81f5c4d1c49f12d61dbcc22b8f8b2a23eb
Andrey Panasenko <honeywithandrew@gmail.com>
2018-11-20 18:52:38 +0300

    Added SCSS
    и отредактирован index.html в соответствии с изменениями в сборке

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,19 @@ var less = require('gulp-less');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
 var autoprefixer = require('gulp-autoprefixer');
+var scss = require('gulp-sass');
 
 
 
 /* Задачи для Gulp */
-gulp.task('server', function() {
+gulp.task('server', ['scss'], function() {
     browserSync.init({
     	server: { baseDir: "./src/" }
     });
 
     gulp.watch('src/**/*.html').on('change', browserSync.reload);
-    gulp.watch('src/less/**/*.less', ['less']);
+    // gulp.watch('src/less/**/*.less', ['less']);
+    gulp.watch('src/scss/**/*.scss', ['scss']);
     gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
 
 });
@@ -31,6 +33,25 @@ gulp.task('less', function() {
     		})
     	}))
     	.pipe(less())
+    	.pipe(autoprefixer({
+    		browsers: ['last 5 versions'],
+    		cascade: false
+    	}))
+    	.pipe(gulp.dest('./src/css'))
+    	.pipe(browserSync.stream());
+});
+
+gulp.task('scss', function() {
+    return gulp.src('./src/scss/main.scss')
+    	.pipe(plumber({
+    		errorHandler: notify.onError(function(err){
+    			return {
+    				title: 'Styles',
+    				message: err.message
+    			}
+    		})
+    	}))
+    	.pipe(scss())
     	.pipe(autoprefixer({
     		browsers: ['last 5 versions'],
     		cascade: false
