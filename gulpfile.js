42ebb0f4c7a1664356e225bf62369a91c388ed9b
Andrey Panasenko <honeywithandrew@gmail.com>
2018-11-21 23:53:49 +0300

    Pug commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,62 +7,80 @@ var notify = require('gulp-notify');
 var autoprefixer = require('gulp-autoprefixer');
 var scss = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var pug = require('gulp-pug');
 
 
 
 /* Задачи для Gulp */
 gulp.task('server', ['scss'], function() {
-    browserSync.init({
-    	server: { baseDir: "./src/" }
-    });
+		browserSync.init({
+			server: { baseDir: "./build/" }
+		});
 
-    gulp.watch('src/**/*.html').on('change', browserSync.reload);
-    // gulp.watch('src/less/**/*.less', ['less']);
-    gulp.watch('src/scss/**/*.scss', ['scss']);
-    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
+		gulp.watch('src/**/*.html').on('change', browserSync.reload);
+		// gulp.watch('src/less/**/*.less', ['less']);
+		gulp.watch('src/scss/**/*.scss', ['scss']);
+		gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
 
 });
 
 gulp.task('less', function() {
-    return gulp.src('./src/less/main.less')
-    	.pipe(plumber({
-    		errorHandler: notify.onError(function(err){
-    			return {
-    				title: 'Styles',
-    				message: err.message
-    			}
-    		})
-    	}))
-    	.pipe(sourcemaps.init())
-    	.pipe(less())
-    	.pipe(autoprefixer({
-    		browsers: ['last 5 versions'],
-    		cascade: false
-    	}))
-    	.pipe(sourcemaps.write())
-    	.pipe(gulp.dest('./src/css'))
-    	.pipe(browserSync.stream());
+		return gulp.src('./src/less/main.less')
+			.pipe(plumber({
+				errorHandler: notify.onError(function(err){
+					return {
+						title: 'Styles',
+						message: err.message
+					}
+				})
+			}))
+			.pipe(sourcemaps.init())
+			.pipe(less())
+			.pipe(autoprefixer({
+				browsers: ['last 5 versions'],
+				cascade: false
+			}))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest('./src/css'))
+			.pipe(browserSync.stream());
 });
 
 gulp.task('scss', function() {
-    return gulp.src('./src/scss/main.scss')
-    	.pipe(plumber({
-    		errorHandler: notify.onError(function(err){
-    			return {
-    				title: 'Styles',
-    				message: err.message
-    			}
-    		})
-    	}))
-    	.pipe(sourcemaps.init())
-    	.pipe(scss())
-    	.pipe(autoprefixer({
-    		browsers: ['last 5 versions'],
-    		cascade: false
-    	}))
-    	.pipe(sourcemaps.write())
-    	.pipe(gulp.dest('./src/css'))
-    	.pipe(browserSync.stream());
+		return gulp.src('./src/scss/main.scss')
+			.pipe(plumber({
+				errorHandler: notify.onError(function(err){
+					return {
+						title: 'Styles',
+						message: err.message
+					}
+				})
+			}))
+			.pipe(sourcemaps.init())
+			.pipe(scss())
+			.pipe(autoprefixer({
+				browsers: ['last 5 versions'],
+				cascade: false
+			}))
+			.pipe(sourcemaps.write())
+			.pipe(gulp.dest('./src/css'))
+			.pipe(browserSync.stream());
+});
+
+gulp.task('pug', function() {
+		return gulp.src('./src/pug/pages/**/*.pug')
+			.pipe(plumber({
+				errorHandler: notify.onError(function(err){
+					return {
+						title: 'Pug',
+						message: err.message
+					}
+				})
+			}))
+			.pipe(pug({
+				pretty: true
+			}))
+			.pipe(gulp.dest('./build'))
+			.pipe(browserSync.stream());
 });
 
 gulp.task('default', ['server']);
